6a46ec240b454ac9c8a78a4d522fbe9fcc1bdb95
Zam0411 <mzambrano41165@gmail.com>
2019-01-29 20:49:22 -0800

    HW

--- assets/giphy.js
+++ assets/giphy.js
@@ -41,7 +41,7 @@ function removeLastButton(){
 // Function that displays all of the gifs
 function displayGifs(){
     var animal = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=fLyYYE4nvcKjckNUfuRidXojJ5OOHGgM";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=fLyYYE4nvcKjckNUfuRidXojJ5OOHGgM";
     console.log(queryURL); // displays the constructed url
     $.ajax({
         url: queryURL,
